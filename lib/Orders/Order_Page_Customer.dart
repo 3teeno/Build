38a162e443ba0970d33c204527e4c87460a15bfc
furqanabaid <furqanabaid@gmail.com>
2022-06-14 01:26:52 +0500

    pop up is added and add button is removed

--- lib/Orders/Order_Page_Customer.dart
+++ lib/Orders/Order_Page_Customer.dart
@@ -14,88 +14,100 @@ class Order_Page_Customer extends StatefulWidget {
   @override
   State<Order_Page_Customer> createState() => _Order_Page_CustomerState();
 }
+
 class _Order_Page_CustomerState extends State<Order_Page_Customer> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            leading: IconButton(icon:Icon(Icons.arrow_back),onPressed: ()=>Navigator.pop(context) ,),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
             elevation: 0,
             title: Text("Manage Orders"),
             backgroundColor: Color(0xFF115ba6),
-      ),
-            floatingActionButton: FloatingActionButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>add_OrderFireBase()));
-            },
-            child: Icon(Icons.add,color: Colors.white,),foregroundColor: Color(0xFF115ba6)),
-            body: Center(
-                child: GridView.extent(
-                  primary: false,
-                  padding: const EdgeInsets.all(16),
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  maxCrossAxisExtent: 200.0,
-                children: <Widget>[
-                  //Active Orders
-                  InkWell(
-                    splashColor: Color(0xFF115ba6),
-                    onTap: () async {
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Active_Orders_Page_Customer(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            "assets/images/activeOrder.png",
-                            height: 70,
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text('Active Orders',
-                              style: TextStyle(fontSize: 20, fontFamily: 'Poppins'))
-                        ],
-                      ),
-                      decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.9),
-                          borderRadius: BorderRadius.all(Radius.circular(5)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 1,
-                              blurRadius: 20,
-                              offset: Offset(0, 3),
-                            ),
-                          ]),
+          ),
+          // floatingActionButton: FloatingActionButton(
+          //     onPressed: () {
+          //       Navigator.push(
+          //           context,
+          //           MaterialPageRoute(
+          //               builder: (context) => add_OrderFireBase()));
+          //     },
+          //     child: Icon(
+          //       Icons.add,
+          //       color: Colors.white,
+          //     ),
+          //     foregroundColor: Color(0xFF115ba6)),
+          body: Center(
+              child: GridView.extent(
+            primary: false,
+            padding: const EdgeInsets.all(16),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            maxCrossAxisExtent: 200.0,
+            children: <Widget>[
+              //Active Orders
+              InkWell(
+                splashColor: Color(0xFF115ba6),
+                onTap: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Active_Orders_Page_Customer(),
                     ),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/activeOrder.png",
+                        height: 70,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text('Active Orders',
+                          style: TextStyle(fontSize: 20, fontFamily: 'Poppins'))
+                    ],
                   ),
-                  //Pending Orders
-                  InkWell(
-                    splashColor: Colors.green,
-                    onTap: () async {
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => pendingOrdersPage(),
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.9),
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 20,
+                          offset: Offset(0, 3),
                         ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
+                      ]),
+                ),
+              ),
+              //Pending Orders
+              InkWell(
+                splashColor: Colors.green,
+                onTap: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => pendingOrdersPage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
                         Image.asset(
                           "assets/images/pendingOrders.png",
                           height: 70,
@@ -104,60 +116,64 @@ class _Order_Page_CustomerState extends State<Order_Page_Customer> {
                           height: 20,
                         ),
                         Text('Pending Orders',
-                            style: TextStyle(fontSize: 20, fontFamily: 'Poppins'))
+                            style:
+                                TextStyle(fontSize: 20, fontFamily: 'Poppins'))
                       ]),
-                      decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.9),
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 1,
-                              blurRadius: 20,
-                              offset: Offset(0, 3),
-                            ),
-                          ]),
-                    ),
-                  ),
-                  //Completed
-                  InkWell(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Delivered_Orders_Page()));
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              "assets/images/completed.png",
-                              height: 70,
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('Completed Orders',
-                                style: TextStyle(fontSize: 20, fontFamily: 'Poppins'),textAlign: TextAlign.center,)
-                          ]),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 1,
-                              blurRadius: 20,
-                              offset: Offset(0, 3),
-                            ),
-                          ]),
-                    ),
-                  ),
-
-
-
-                    ],
-                  )),
-                ));
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.9),
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 20,
+                          offset: Offset(0, 3),
+                        ),
+                      ]),
+                ),
+              ),
+              //Completed
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Delivered_Orders_Page()));
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/images/completed.png",
+                          height: 70,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Completed Orders',
+                          style: TextStyle(fontSize: 20, fontFamily: 'Poppins'),
+                          textAlign: TextAlign.center,
+                        )
+                      ]),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 20,
+                          offset: Offset(0, 3),
+                        ),
+                      ]),
+                ),
+              ),
+            ],
+          )),
+        ));
   }
 }
