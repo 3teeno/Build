d9f037fe7d760aeffaeb6e0165552fe47c1c1151
furqanabaid <furqanabaid@gmail.com>
2022-06-27 04:21:51 +0500

    Search Page Complete

--- lib/all_market_places/Search_Material.dart
+++ lib/all_market_places/Search_Material.dart
@@ -6,16 +6,56 @@ import 'package:build_i_t/home_page/marketPlaceCard.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../Products/Model_Products.dart';
 import '../data.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final FirebaseAuth auth = FirebaseAuth.instance;
 final User user = auth.currentUser;
+List<Products> products = [];
+
+
+void initState() {
+initialise();
+
+
+}
+bool x=true;
+
+List my_products = [];
+Future<void> initialise() async {
+  await Products.fetch_product().then((value)   {
+    my_products = value;
+    print(value);
+    for (var i = 0; i < my_products.length; i++) {
+      Products temp = Products(
+        Product_Hash: my_products[i]['Product_Hash'],
+        Product_Title: my_products[i]['Product_Title'],
+        Product_Description: my_products[i]['Product_Description'],
+        Product_Quantity: my_products[i]['Product_Quantity'],
+        Product_Price: my_products[i]['Product_Price'],
+        Product_Vendor_id: my_products[i]['Product_Vendor_id'],
+      );
+      products.add(temp);
+      print("Products");
+    }
+  });
+  print("Maa aa gaya");
+  //print(my_products);
+  if (my_products.isEmpty) {
+    print("Empty Products Against Vendor");
+  }}
+
+
 
 class DataSearch extends SearchDelegate<String> {
+
+
   @override
   ThemeData appBarTheme(BuildContext context) {
     // TODO: implement appBarTheme
+    print(products);
+    //initialise();
     return ThemeData(
         appBarTheme: const AppBarTheme(
           backgroundColor: Color(0xFF115ba6),
@@ -34,6 +74,7 @@ class DataSearch extends SearchDelegate<String> {
         ),
         inputDecorationTheme:
             new InputDecorationTheme(border: InputBorder.none));
+
   }
 
   Random random = new Random();
@@ -60,6 +101,7 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   Widget buildLeading(BuildContext context) {
+    //initialise();
     return IconButton(
         onPressed: () {
           close(context, null);
@@ -78,50 +120,55 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    List list2 = data.chatnames();
-    final list = query.isEmpty
-        ? list2
-        : list2
-            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
-            .toList();
-    print(data);
+if(x==true){
+  initialise();
+  x=false;
+}
 
+
+    print("vlaue:"+products.length.toString());
+    List data=[];
+    for (int i=1;i<products.length;i++){
+      data.add(products[i].Product_Title);
+    }
+    final list=query.isEmpty? data:data.where((p)=>p.toLowerCase().startsWith(query.toLowerCase())).toList();
+//print("length: "+list.length.toString());
     return ListView.builder(
         itemCount: list.length,
         itemBuilder: (context, index) => marketPlaceCard(
-            title: list[index],
+            title: list[index].toString(),
             imageUrl:
-                'https://images.unsplash.com/photo-1541888946425-d81bb19240f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-            price: "PKR 500",
-            quantity: "5",
+            "https://images.pexels.com/photos/159306/construction-site-build-construction-work-159306.jpeg?cs=srgb&dl=pexels-pixabay-159306.jpg&fm=jpg",
+            price: products[index].Product_Price,
+            quantity: products[index].Product_Quantity,
             context: context,
-            description: data.description()));
-    throw UnimplementedError();
+            description: products[index].Product_Description.toString()));
+    //throw UnimplementedError();
   }
 
-  Future<List> querySnapShot() async {
-    List docs = [];
-    await FirebaseFirestore.instance
-        .collection('products')
-        .get()
-        .then((value) => {
-              print(value.docs.first.data()),
-              value.docs.toList().forEach((doc) {
-                print(doc.data());
-                Map my_products = {
-                  "Product_Hash": doc['Product_Hash'],
-                  "Product_Title": doc['Product_Title'],
-                  "Product_Description": doc['Product_Description'],
-                  "Product_Quantity": doc['Product_Quantity'],
-                  "Product_Price": doc['Product_Price'],
-                  "Product_Vendor_id": doc['Product_Vendor_id'],
-                  "Product_Category": doc['Product_Category'],
-                  "Product_Images": doc['Product_Images'],
-                };
-                docs.add(my_products);
-                print(docs);
-              })
-            });
-    return docs;
-  }
+  // Future<List> querySnapShot() async {
+  //   List docs = [];
+  //   await FirebaseFirestore.instance
+  //       .collection('products')
+  //       .get()
+  //       .then((value) => {
+  //             print(value.docs.first.data()),
+  //             value.docs.toList().forEach((doc) {
+  //               print(doc.data());
+  //               Map my_products = {
+  //                 "Product_Hash": doc['Product_Hash'],
+  //                 "Product_Title": doc['Product_Title'],
+  //                 "Product_Description": doc['Product_Description'],
+  //                 "Product_Quantity": doc['Product_Quantity'],
+  //                 "Product_Price": doc['Product_Price'],
+  //                 "Product_Vendor_id": doc['Product_Vendor_id'],
+  //                 "Product_Category": doc['Product_Category'],
+  //                 "Product_Images": doc['Product_Images'],
+  //               };
+  //               docs.add(my_products);
+  //               print(docs);
+  //             })
+  //           });
+  //   return docs;
+  // }
 }
